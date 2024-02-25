99b17521fb7809935d74ce1fb47c8950710787fc
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2024-02-25 10:29:40 +0000

    Working but imperfect voxel display, needs more work on opacity and colours

--- display_voxels.py
+++ display_voxels.py
@@ -23,11 +23,12 @@ def load_peptide(pdb_code: str):
     cmd.load(histo_peptide_url)
 
 
-def display_pseudoatom(name:str, pos:List[float], color:str='gray80', sphere_scale:float=0.3):
+def display_pseudoatom(name:str, pos:List[float], color:str='gray80', sphere_scale:float=0.3, opacity:int=1):
     cmd.pseudoatom(name, pos=pos)
     cmd.show('spheres',name)
     cmd.color(color,name)
     cmd.set ("sphere_scale", sphere_scale, name)
+    cmd.set ("sphere_transparency", opacity, name)
 
 
 def show_axis_voxel(voxel_coords:List, decimation_factor:int):
@@ -54,7 +55,7 @@ def show_axis_voxel(voxel_coords:List, decimation_factor:int):
 def display_voxel_box(centre_of_mass:List[float], box_xyz:List[float], voxel_dict:Dict):
 
     display_pseudoatom('first_voxel', voxel_dict['voxels'][voxel_dict['labels'][0]]['start'], color='white')
-    display_pseudoatom('last_voxel', voxel_dict['voxels'][voxel_dict['labels'][voxel_dict['params']['count'] - 1]]['start'], color='gray80')
+    display_pseudoatom('last_voxel', voxel_dict['voxels'][voxel_dict['labels'][voxel_dict['params']['voxel_count'] - 1]]['start'], color='gray80')
 
     for key in voxel_dict['voxels']:
         x, y, z = key.split('_')
@@ -66,11 +67,12 @@ def display_voxel_box(centre_of_mass:List[float], box_xyz:List[float], voxel_dic
     for axis in ['x', 'y', 'z']:
         cmd.set ('sphere_transparency', 1, f'voxel_grid_{axis}')
 
+voxel_map_hash = 'e91d9bdc62da8457549cfbeed4c2b0aa'
+
+voxel_grid = json.load(open(f"output/voxel_sets/{voxel_map_hash}/voxel_set.json", 'r'))
 
 load_mhc_abd(constants.canonical_pdb_code)
 load_peptide(constants.canonical_pdb_code)
-
-voxel_grid = functions.create_voxel_grid(constants.centre_of_mass, constants.box_xyz, constants.voxel_size, range_offset=1, y_offset=8)
 
 display_pseudoatom('centre_of_box', constants.centre_of_mass, color='white')
 
@@ -78,16 +80,38 @@ display_voxel_box(constants.centre_of_mass, constants.box_xyz, voxel_grid)
 
 pdb_code = constants.canonical_pdb_code
 
+position_voxels = json.load(open(f"output/voxel_sets/{voxel_map_hash}/position_voxels.json", 'r'))
 
-for pdb_code in constants.test_pdb_codes:
+print (position_voxels)
 
-    filename = f"output/voxels/{pdb_code}.json"
+for position in position_voxels:
 
-    with open(filename, 'r') as filehandle:
-        structure_voxels = json.load(filehandle)
+    voxel_labels = position_voxels[position].keys()
 
-    for position in structure_voxels:
-        display_pseudoatom(f"{pdb_code}_voxels", structure_voxels[position]['voxel_centre'], color='green', sphere_scale=1)
-        display_pseudoatom(f"p{position}_voxels", structure_voxels[position]['voxel_centre'], color='red', sphere_scale=1)
+    voxel_counts = [position_voxels[position][label]['count'] for label in voxel_labels]
+
+    voxel_total = sum(voxel_counts)
+
+    voxel_percentages = [round(count / voxel_total *100, 2) for count in voxel_counts]
+
+    voxel_colours = [f"s{round(percentage *10):03}" for percentage in voxel_percentages]
+
+    voxel_opacity = [0.6 - (int((percentage * 0.1) + 1)) / 10  for percentage in voxel_percentages]
+    print (voxel_colours)
+    
+    i = 0
+    for voxel_label in voxel_labels:
+        print (voxel_label)
+        print (voxel_opacity[i])
+        display_pseudoatom(f"p{position}_{voxel_label}", voxel_grid['voxels'][voxel_label]['centre'], color=voxel_colours[i], sphere_scale=0.7, opacity=voxel_opacity[i])
+
+
+        i += 1
+
+        
+
+#    for position in structure_voxels:
+#        display_pseudoatom(f"{pdb_code}_voxels", structure_voxels[position]['voxel_centre'], color='green', sphere_scale=1)
+#        display_pseudoatom(f"p{position}_voxels", structure_voxels[position]['voxel_centre'], color='red', sphere_scale=1)
 
 cmd.reset()
