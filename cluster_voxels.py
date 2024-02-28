53179360375e0d5d7599956d38ad92cc8bf06e6c
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2024-02-28 09:15:35 +0000

    Revised version of clustering which gives more information about the clusters

--- cluster_voxels.py
+++ cluster_voxels.py
@@ -19,7 +19,7 @@ labels = list(used_voxels.keys())
 
 all_positions_data = [tensorize(voxel_labels) for voxel_labels in used_voxels.values()]
 
-positions_for_clustering = [5, 6]
+positions_for_clustering = [4, 5, 6]
 min_cluster_size = 3
 
 clustering_data = []
@@ -39,7 +39,6 @@ for row in all_positions_data:
 hdb = HDBSCAN(min_cluster_size=min_cluster_size)
 hdb.fit(clustering_data)
 
-print (f"Clustering at positions {positions_for_clustering} with a minimum cluster size of {min_cluster_size} gives {len(set(hdb.labels_)) - 1} meaningful clusters and a group of outliers.")
 
 clusters = {}
 
@@ -49,11 +48,59 @@ for i in range(len(hdb.labels_)):
         clusters[cluster] = []
     clusters[cluster].append(labels[i])
 
+print (f"Clustering at positions {positions_for_clustering} with a minimum cluster size of {min_cluster_size} gives {len(set(hdb.labels_)) - 1} meaningful clusters and a group of outliers with {len(clusters[0])} members.")
 
-for cluster in clusters:
-    print (cluster, len(clusters[cluster]))
+print ("The clusters are as follows:")
+
+structure_info = json.load(open('output/structure_information/all.json', 'r'))
+
+alleles = {}
+
+for key, value in sorted(clusters.items(), key=lambda item: int(item[0])):
+    print (f"\nCluster {key}: {len(value)} members")
+
+    
+
+    if key != 0:
+        cluster_alleles = {}
+        cluster_used_voxels = {}
+        for i in range(1,10):
+            cluster_used_voxels[i] = {}
+        print (value)
+        for member in value:
+            
+            this_used_voxels = used_voxels[member]
+
+            i = 1
+            for voxel in this_used_voxels:
+                if not voxel in cluster_used_voxels:
+                    cluster_used_voxels[i][voxel] = 0
+                cluster_used_voxels[i][voxel] += 1
+                i += 1
+
+            this_allele = structure_info['structures'][member]['allele']
+            if this_allele not in alleles:
+                alleles[this_allele] = []
+            if key not in alleles[this_allele]:
+                alleles[this_allele].append(key)
+            if this_allele not in cluster_alleles:
+                cluster_alleles[this_allele] = 0
+            cluster_alleles[this_allele] += 1
+        print (f"Allele: {cluster_alleles}")
+        print (f"\nVoxels:")
+        for i in range(1,10):
+            print (f"Position {i}: {len(cluster_used_voxels[i])} voxels")
+
+print ("\n\nAllele cluster distribution:") 
+for allele in alleles:
+    print (f"{allele}: {len(alleles[allele])} clusters {alleles[allele]}")     
+
 
 
 positions_for_clustering = [str(p) for p in positions_for_clustering]
 
-json.dump(clusters, open(f"output/clusters/{voxel_map_hash}__{min_cluster_size}__{'_'.join(positions_for_clustering)}.json", 'w'), indent=4)
+filename = f"output/clusters/{voxel_map_hash}__{min_cluster_size}__{'_'.join(positions_for_clustering)}.json"
+json.dump(clusters, open(filename, 'w'), indent=4)
+
+print (f"The data for this run can be found in {filename}")
+
