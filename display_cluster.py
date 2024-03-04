0cde6778c58204fa3dee28d99e57381b0f5ef6f1
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2024-03-04 12:04:30 +0000

    Added the ability to list clusters in arguments passed to Pymol and then on to the function

--- display_cluster.py
+++ display_cluster.py
@@ -1,7 +1,18 @@
 from pymol import cmd
 
 import json
+import sys
 
+print (sys.argv)
+
+cluster_numbers = []
+
+for i in sys.argv[1:]: 
+    if i.isdigit():
+        cluster_numbers.append(int(i))
+
+if len(cluster_numbers) == 0:
+    cluster_numbers = [1]
 
 structure_info = json.load(open('output/structure_information/all.json', 'r'))
 
@@ -10,13 +21,12 @@ def load_structure(pdb_code):
     cmd.load(f"structures/{pdb_code}_peptide.pdb")
 
 voxel_map_hash = 'e91d9bdc62da8457549cfbeed4c2b0aa'
+min_cluster_size = 3
 
-used_voxels = json.load(open(f"output/voxel_sets/{voxel_map_hash}/used_voxels.json", 'r'))
+#used_voxels = json.load(open(f"output/voxel_sets/{voxel_map_hash}/used_voxels.json", 'r'))
 
-clusters = json.load(open(f"output/clusters/{voxel_map_hash}__3__5_6.json", 'r'))
+clusters = json.load(open(f"output/clusters/{voxel_map_hash}/cluster_size_{min_cluster_size}/clustering_4_5_6.json", 'r'))
 
-cluster_numbers = [10, 11, 12]
-#cluster_numbers = [1,2,3]
 
 #filter = 'hla_b_07_02'
 filter = None
@@ -29,7 +39,7 @@ print ("Showing structures for cluster(s):", cluster_numbers)
 for cluster_number in cluster_numbers:
     print (f"Cluster {cluster_number}")
     i = 0
-    for pdb_code in clusters[str(cluster_number)]:
+    for pdb_code in clusters['clusters'][str(cluster_number)]:
         if not filter:
             load_structure(pdb_code)
         elif filter in structure_info['structures'][pdb_code]['allele_slug']:
